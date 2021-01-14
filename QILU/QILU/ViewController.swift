eb5fd8e4e7c225006c3b8f7ac5140803f2cf8ead
huanyu.li <lihuanyux@163.com>
2021-01-14 10:36:26 +0800

    初始化项目

---
+++ QILU/QILU/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  QILU
+//
+//  Created by huanyu.li on 2021/1/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
